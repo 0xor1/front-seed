4fd641074f5e0eab0f90afe05d3e080ec0e3a793
daniel robinson <daniel.robinson@teamwork.com>
2016-08-30 20:28:15 +0100

    remove js files

--- main.dev.js
+++
@@ -1,4 +0,0 @@
-define(["require", "exports", '@angular/platform-browser-dynamic', 'registry'], function (require, exports, platform_browser_dynamic_1, registry_1) {
-    "use strict";
-    platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(registry_1.Registry);
-});
--- main.prod.js
+++
@@ -1,5 +0,0 @@
-define(["require", "exports", '@angular/platform-browser-dynamic', 'registry', '@angular/core'], function (require, exports, platform_browser_dynamic_1, registry_1, core_1) {
-    "use strict";
-    core_1.enableProdMode();
-    platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(registry_1.Registry);
-});
